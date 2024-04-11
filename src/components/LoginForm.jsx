2ff00b4bdbfecdaf77699f86c265d0639142d9c8
LeanApa <lea.apagro@gmail.com>
2024-04-10 22:19:18 -0300

    agrego login por api

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -13,28 +13,57 @@ import {
 import { EyeFilledIcon } from "./EyeFilledIcon";
 import { EyeSlashFilledIcon } from "./EyeSlashFilledIcon";
 import { ReciclarLogo } from "./ReciclarLogo";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isVisible, setIsVisible] = React.useState(false);
   const toggleVisibility = () => setIsVisible(!isVisible);
+  const [isInvalid, setIsInvalid] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Aquí puedes enviar los datos del formulario a tu servidor para autenticación
-    console.log("Email:", email);
-    console.log("Password:", password);
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      // Aquí puedes enviar los datos del formulario a tu servidor para autenticación
+      console.log("Email:", email);
+      console.log("Password:", password);
+      const response = await fetch(`${apiUrl}/sessions/loginadmin`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.message) {
+        setIsInvalid(true);
+        setErrorMessage(data.message);
+        return; // Salir de la función si hay un error
+      } else {
+        setIsInvalid(false);
+        setErrorMessage("");
+      }
+
+      const { accessToken } = data;
+      sessionStorage.setItem("accessToken", accessToken);
+    } catch (error) {
+      console.error(error);
+    }
+
     // Luego puedes redirigir al usuario a la página principal, por ejemplo
   };
   return (
     <form onSubmit={handleSubmit}>
       <Card className="max-w-[full]">
         <CardHeader className="flex gap-3">
-          <ReciclarLogo/>
+          <ReciclarLogo />
           <div className="flex flex-col">
             <p className="text-md">Reciclar Admin</p>
             <p className="text-small text-default-500">Inicie sesión</p>
@@ -50,6 +79,7 @@ const LoginForm = () => {
               onChange={handleEmailChange}
               isRequired
               fullWidth
+              isInvalid={isInvalid}
             />
           </div>
           <div style={{ marginBottom: "1rem" }}>
@@ -58,6 +88,8 @@ const LoginForm = () => {
               onChange={handlePasswordChange}
               fullWidth
               isRequired
+              isInvalid={isInvalid}
+              errorMessage={errorMessage}
               endContent={
                 <button
                   className="focus:outline-none"
