12ed4db90e0f70cf0c9b6f5d545e81ea2f1b8cd9
LeanApa <lea.apagro@gmail.com>
2024-04-11 21:16:12 -0300

    mejoras al boton de login

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,6 +14,7 @@ import { ReciclarLogo } from "./ReciclarLogo.jsx";
 
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const isLoggedIn = sessionStorage.getItem("accessToken");
 
   const menuItems = ["Empresas", "Usuarios", "Posts", "Reciclables", "LogOut"];
 
@@ -36,9 +37,14 @@ export default function NavBar() {
         </NavbarBrand>
       </NavbarContent>
       <NavbarContent justify="end">
-        <NavbarItem className="lg:flex">
-          <Link href="/login">Login</Link>
-        </NavbarItem>
+        {!isLoggedIn ? (
+          <NavbarItem className="lg:flex">
+            <Link href="/login">Login</Link>
+          </NavbarItem>
+        ) : (
+          ""
+        )}
+
         {/* <NavbarItem>
           <Button as={Link} color="primary" href="#" variant="flat">
             Sign Up
