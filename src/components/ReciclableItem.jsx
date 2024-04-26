c47aad9ce4730bb4e28e0197d243ef31936ac00e
LeanApa <lea.apagro@gmail.com>
2024-04-25 21:21:43 -0300

    agrego page de reciclables

--- src/components/ReciclableItem.jsx
+++ src/components/ReciclableItem.jsx
@@ -20,46 +20,35 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 const ReciclableItem = ({ reciclable, isLoggedIn }) => {
   const [title, setTitle] = useState("");
-  const [subtitle, setSubTitle] = useState("");
-  const [category, setCategory] = useState("");
   const [imageUrl, setImageUrl] = useState("");
-  const [level, setLevel] = useState("");
-  const [content, setContent] = useState("");
+  const [description, setDescription] = useState("");
 
   const navigate = useNavigate();
-  const levels = [
-    { description: "PRINCIPIANTE" },
-    { description: "INTERMEDIO" },
-    { description: "AVANZADO" },
-  ];
 
   const handleTitleChange = (e) => setTitle(e.target.value);
-  const handleSubTitleChange = (e) => setSubTitle(e.target.value);
-  const handleCategoryChange = (e) => setCategory(e.target.value);
   const handleImageUrlChange = (e) => setImageUrl(e.target.value);
-  const handleLevelChange = (e) => setLevel([e.currentKey]);
-  const handleContentChange = (e) => setContent(e.target.value);
+  const handleDescriptionChange = (e) => setDescription(e.target.value);
 
   const handleSubmit = async (e) => {
     try {
       e.preventDefault();
 
       if (reciclable) {
-        const response = await fetch(`${API_URL}/reciclables/${reciclable._id}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            accessToken: `${isLoggedIn}`,
-          },
-          body: JSON.stringify({
-            title: title,
-            subtitle: subtitle,
-            category: category,
-            imageUrl: imageUrl,
-            level: level,
-            content: content,
-          }),
-        });
+        const response = await fetch(
+          `${API_URL}/reciclables/${reciclable._id}`,
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              accessToken: `${isLoggedIn}`,
+            },
+            body: JSON.stringify({
+              title: title,
+              imageUrl: imageUrl,
+              description: description,
+            }),
+          }
+        );
       } else {
         const response = await fetch(`${API_URL}/reciclables/`, {
           method: "POST",
@@ -69,11 +58,8 @@ const ReciclableItem = ({ reciclable, isLoggedIn }) => {
           },
           body: JSON.stringify({
             title: title,
-            subtitle: subtitle,
-            category: category,
             imageUrl: imageUrl,
-            level: level,
-            content: content,
+            description: description,
           }),
         });
       }
@@ -96,7 +82,9 @@ const ReciclableItem = ({ reciclable, isLoggedIn }) => {
               <Avatar name={reciclable?.title} />
               <div className="flex flex-col">
                 <p className="text-md">{reciclable?.title}</p>
-                <p className="text-small text-default-500">{reciclable?.subtitle}</p>
+                <p className="text-small text-default-500">
+                  {reciclable?.description}
+                </p>
               </div>
             </>
           )}
@@ -113,48 +101,18 @@ const ReciclableItem = ({ reciclable, isLoggedIn }) => {
           </div>
           <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-4">
             <Input
-              onChange={handleSubTitleChange}
-              type="text"
-              label="Sub título"
-              placeholder={reciclable?.subtitle || "Ingrese un subtítulo"}
-            />
-          </div>
-          <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-4">
-            <Input
-              onChange={handleCategoryChange}
-              type="text"
-              label="Categoría"
-              placeholder={reciclable?.category || "Ingrese una categoría"}
-            />
-          </div>
-          <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-4">
-            <Input
               onChange={handleImageUrlChange}
               type="text"
               label="URL de imagen"
               placeholder={reciclable?.imageUrl || "Ingrese una URL"}
             />
           </div>
-          <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-4">
-            <Select
-              items={levels}
-              label="Nivel"
-              placeholder={reciclable?.level || "Elija el nivel"}
-              selectedKeys={level}
-              onSelectionChange={handleLevelChange}
-            >
-              {levels.map((level) => (
-                <SelectItem key={level.description}>
-                  {level.description}
-                </SelectItem>
-              ))}
-            </Select>
-          </div>
+
           <div className="flex w-full flex-wrap md:flex-nowrap gap-4 mt-4">
             <Textarea
-              onChange={handleContentChange}
+              onChange={handleDescriptionChange}
               label="Contenido"
-              placeholder={reciclable?.content || "Escriba el contenido"}
+              placeholder={reciclable?.description || "Escriba el contenido"}
             />
           </div>
         </CardBody>
