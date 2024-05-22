ddba0e3c6f0293e1f1c9ba55f3af6273a30ca701
LeanApa <lea.apagro@gmail.com>
2024-05-22 20:14:30 -0300

    cambio en loading

--- src/pages/Empresas.jsx
+++ src/pages/Empresas.jsx
@@ -103,6 +103,7 @@ const Empresas = ({ isLoggedIn }) => {
         });
         const data = await response.json();
         setCompanies(data); // Establecer el estado dentro del try
+        setIsLoading(!isLoading);
       } catch (error) {
         console.log(error);
       }
